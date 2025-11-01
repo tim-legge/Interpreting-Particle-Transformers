5bb0530f8847f77c613778cbe9a478f336442007
tim_legge <tlegge@ucsd.edu>
2025-11-01 00:05:41 -0700

    save state dict instead of model

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DATADIR_JetClass=
+export DATADIR_JetClass=/moe-interpretability-pv/datasets/JetClass
 export DATADIR_TopLandscape=
 export DATADIR_QuarkGluon=
 export DATADIR_HLS4ML=
