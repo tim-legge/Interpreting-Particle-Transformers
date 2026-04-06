73c0ad8b61e374e8133b4fc7b77cef4798663e35
tim_legge <tlegge@ucsd.edu>
2026-04-06 15:13:33 -0700

    update training and cls token analysis

--- train_JetClass.sh
+++ train_JetClass.sh
@@ -32,6 +32,9 @@ model=$1
 if [[ "$model" == "ParT" ]]; then
     modelopts="networks/example_ParticleTransformer.py --use-amp"
     batchopts="--batch-size 512 --start-lr 1e-3"
+elif [[ "$model" == "Argmax-ParT" ]]; then
+    modelopts="networks/ParT_argmax.py --use-amp"
+    batchopts="--batch-size 512 --start-lr 1e-3"
 elif [[ "$model" == "PN" ]]; then
     modelopts="networks/example_ParticleNet.py"
     batchopts="--batch-size 512 --start-lr 1e-2"
